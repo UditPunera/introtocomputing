5457ba0da793cc0170f371e50321716af23bcebc
UditPunera <104358934+UditPunera@users.noreply.github.com>
2022-04-25 16:47:26 +0530

    Add files via upload

---
+++ output5.py
@@ -0,0 +1,13 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: /Users/uditpunera/Desktop/code5.py =================
+
+
+color: ['Red', 'Green', 'White', 'Pink', 'Yellow']  #5a
+
+
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow'] #5b
+
+
+#Q5 output a and b
