08d0bbdf2d9684ef257072f82f028f0c6311a40c
UditPunera <104358934+UditPunera@users.noreply.github.com>
2022-05-17 16:42:30 +0530

    Delete output2.py

--- output2.py
+++
@@ -1,7 +0,0 @@
-Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-
-==================== RESTART: /Users/uditpunera/Desktop/2.py ===================
-Hey, Udit Punera Here!
-My SID is 21102100
-I am from Civil department and my CGPA is 9.9
